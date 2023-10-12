be0c4291ac2de4a5c7ab8a9a3f739c632a67c0c5
hafiz-muhamad <79085875+hafiz-muhamad@users.noreply.github.com>
2023-10-12 09:05:28 +0800

    Update script.js

--- template-1/script.js
+++ template-1/script.js
@@ -1,31 +1,53 @@
 function countdown() {
     let now = new Date();
 
+    let currentYear = now.getFullYear();
     let currentDate = now.getDate();
     let currentMonth = now.getMonth();
     let currentHour = now.getHours();
     let currentMinute = now.getMinutes();
     let currentSecond = now.getSeconds();
 
-    let targetMonth = 9;
-    let targetDate = 2;
+    let targetYear = 2024
+    let targetMonth = 4;
+    let targetDate = 20;
     let targetHour = 16;
     let targetMinute = 60;
     let targetSecond = 60;
     
+    let countYear = 0;
     let countMonth = 0;
     let countDate = 0;
     let countHour = 0;
     let countMinute = 0;
     let countSecond = 0;
 
+    let passed = 0;
 
     //countdown day
     let monthDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     countMonth = targetMonth - (currentMonth+1);
+    countYear = targetYear - currentYear;
     if (countMonth == 0) {
         countDate = targetDate - currentDate
-        console.log("date", countDate)
+    }
+    else if (countMonth < 0) {
+         if (countYear > 0) {
+            let temp = monthDay[currentMonth] - currentDate;
+            for(let i=currentMonth+1; i<12; i++) {
+                temp += monthDay[i];
+            }
+            
+            for(let j=0; j<targetMonth; j++) {
+                temp += monthDay[j];
+            }
+
+            countDate = temp;
+         }
+         else {
+            countDate = 0;
+            passed = 1;
+         }
     }
     else {
         let temp = monthDay[currentMonth] - currentDate;
@@ -39,29 +61,39 @@ function countdown() {
         countDate = temp;
     }
 
-    //countdown hour
-    countHour = targetHour - currentHour - 1;
-    if(countHour < 0) {
-        countHour = 24 + (targetHour - currentHour - 1);
+
+    if (passed == 1) {
+        countHour = 0;
+        countMinute = 0;
+        countSecond = 0;
     }
+    else {
+        //countdown hour
+        countHour = targetHour - currentHour - 1;
+        if(countHour < 0) {
+            countHour = 24 + (targetHour - currentHour - 1);
+        }
 
-    //countdown minute
-    countMinute = targetMinute - currentMinute;
+        //countdown minute
+        countMinute = targetMinute - currentMinute;
 
-    //countdown second
-    countSecond = targetSecond - currentSecond;
+        //countdown second
+        countSecond = targetSecond - currentSecond;
 
-    document.querySelector(".num-day").innerText = countDate;
-    document.querySelector(".num-hour").innerText = countHour;
-    document.querySelector(".num-minute").innerText = countMinute;
-    document.querySelector(".num-second").innerText = countSecond;
-
+        document.querySelector(".num-day").innerText = countDate;
+        document.querySelector(".num-hour").innerText = countHour;
+        document.querySelector(".num-minute").innerText = countMinute;
+        document.querySelector(".num-second").innerText = countSecond;
+    }
+    
+    /*
     if(countDate <= 0) {
         document.querySelector(".num-day").innerText = 0;
         document.querySelector(".num-hour").innerText = 0;
         document.querySelector(".num-minute").innerText = 0;
         document.querySelector(".num-second").innerText = 0;
     }
+    */
 }
 
 countdown()
