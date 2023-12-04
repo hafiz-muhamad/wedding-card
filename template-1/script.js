87dc1d55efc8ed7aa8d3a9e5a24cbe5521b6caee
hafiz-muhamad <79085875+hafiz-muhamad@users.noreply.github.com>
2023-12-04 04:46:33 +0000

    edit countdown

--- template-1/script.js
+++ template-1/script.js
@@ -1,99 +1,99 @@
 function countdown() {
-    let now = new Date();
+  let now = new Date();
 
-    let currentYear = now.getFullYear();
-    let currentDate = now.getDate();
-    let currentMonth = now.getMonth();
-    let currentHour = now.getHours();
-    let currentMinute = now.getMinutes();
-    let currentSecond = now.getSeconds();
+  let currentYear = now.getFullYear();
+  let currentDate = now.getDate();
+  let currentMonth = now.getMonth();
+  let currentHour = now.getHours();
+  let currentMinute = now.getMinutes();
+  let currentSecond = now.getSeconds();
 
-    let targetYear = 2023;
-    let targetMonth = 9;
-    let targetDate = 2;
-    let targetHour = 16;
-    let targetMinute = 60;
-    let targetSecond = 60;
-    
-    let countYear = 0;
-    let countMonth = 0;
-    let countDate = 0;
-    let countHour = 0;
-    let countMinute = 0;
-    let countSecond = 0;
+  let targetYear = 2024;
+  let targetMonth = 9;
+  let targetDate = 2;
+  let targetHour = 16;
+  let targetMinute = 60;
+  let targetSecond = 60;
 
-    let passed = 0;
+  let countYear = 0;
+  let countMonth = 0;
+  let countDate = 0;
+  let countHour = 0;
+  let countMinute = 0;
+  let countSecond = 0;
 
-    //countdown day
-    let monthDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    countMonth = targetMonth - (currentMonth+1);
-    countYear = targetYear - currentYear;
-    if (countMonth == 0) {
-        countDate = targetDate - currentDate
-    }
-    else if (countMonth < 0) {
-         if (countYear > 0) {
-            let temp = monthDay[currentMonth] - currentDate;
-            for(let i=currentMonth+1; i<12; i++) {
-                temp += monthDay[i];
-            }
-            
-            for(let j=0; j<targetMonth; j++) {
-                temp += monthDay[j];
-            }
+  let passed = 0;
 
-            countDate = temp;
-         }
-         else {
-            countDate = 0;
-            passed = 1;
-         }
+  //countdown day
+  let monthDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  countMonth = targetMonth - (currentMonth + 1);
+  countYear = targetYear - currentYear;
+  if (countMonth == 0) {
+    countDate = targetDate - currentDate
+  }
+  else if (countMonth < 0) {
+    if (countYear > 0) {
+      let temp = monthDay[currentMonth] - currentDate;
+      for (let i = currentMonth + 1; i < 12; i++) {
+        temp += monthDay[i];
+      }
+
+      for (let j = 0; j < targetMonth; j++) {
+        temp += monthDay[j];
+      }
+
+      countDate = temp;
     }
     else {
-        let temp = monthDay[currentMonth] - currentDate;
-        if (countMonth > 1) {
-            for(let i=currentMonth+1; i <targetMonth-1; i++){
-                temp += monthDay[i];
-            }
-        }        
-        temp += targetDate;
+      countDate = 0;
+      passed = 1;
+    }
+  }
+  else {
+    let temp = monthDay[currentMonth] - currentDate;
+    if (countMonth > 1) {
+      for (let i = currentMonth + 1; i < targetMonth - 1; i++) {
+        temp += monthDay[i];
+      }
+    }
+    temp += targetDate;
 
-        countDate = temp;
+    countDate = temp;
+  }
+
+
+  if (passed == 1) {
+    countHour = 0;
+    countMinute = 0;
+    countSecond = 0;
+  }
+  else {
+    //countdown hour
+    countHour = targetHour - currentHour - 1;
+    if (countHour < 0) {
+      countHour = 24 + (targetHour - currentHour - 1);
     }
 
+    //countdown minute
+    countMinute = targetMinute - currentMinute;
 
-    if (passed == 1) {
-        countHour = 0;
-        countMinute = 0;
-        countSecond = 0;
-    }
-    else {
-        //countdown hour
-        countHour = targetHour - currentHour - 1;
-        if(countHour < 0) {
-            countHour = 24 + (targetHour - currentHour - 1);
-        }
+    //countdown second
+    countSecond = targetSecond - currentSecond;
 
-        //countdown minute
-        countMinute = targetMinute - currentMinute;
+    document.querySelector(".num-day").innerText = countDate;
+    document.querySelector(".num-hour").innerText = countHour;
+    document.querySelector(".num-minute").innerText = countMinute;
+    document.querySelector(".num-second").innerText = countSecond;
+  }
 
-        //countdown second
-        countSecond = targetSecond - currentSecond;
-
-        document.querySelector(".num-day").innerText = countDate;
-        document.querySelector(".num-hour").innerText = countHour;
-        document.querySelector(".num-minute").innerText = countMinute;
-        document.querySelector(".num-second").innerText = countSecond;
-    }
-    
-    /*
-    if(countDate <= 0) {
-        document.querySelector(".num-day").innerText = 0;
-        document.querySelector(".num-hour").innerText = 0;
-        document.querySelector(".num-minute").innerText = 0;
-        document.querySelector(".num-second").innerText = 0;
-    }
-    */
+  /*
+  if(countDate <= 0) {
+      document.querySelector(".num-day").innerText = 0;
+      document.querySelector(".num-hour").innerText = 0;
+      document.querySelector(".num-minute").innerText = 0;
+      document.querySelector(".num-second").innerText = 0;
+  }
+  */
 }
 
 countdown()
@@ -101,28 +101,28 @@ setInterval(countdown, 1000);
 
 //call menu
 document.querySelector("#call").addEventListener("click", function() {
-    let menu = document.querySelector("#call-menu");
-    
-    menu.style.bottom = "0px"
+  let menu = document.querySelector("#call-menu");
+
+  menu.style.bottom = "0px"
 })
 
 document.querySelector("#close-call").addEventListener("click", function() {
-    let menu = document.querySelector("#call-menu");
-    
-    menu.style.bottom = "-300px"
+  let menu = document.querySelector("#call-menu");
+
+  menu.style.bottom = "-300px"
 })
 
 //location menu
 document.querySelector("#location").addEventListener("click", function() {
-    let menu = document.querySelector("#location-menu");
-    
-    menu.style.bottom = "0px"
+  let menu = document.querySelector("#location-menu");
+
+  menu.style.bottom = "0px"
 })
 
 document.querySelector("#close-location").addEventListener("click", function() {
-    let menu = document.querySelector("#location-menu");
-    
-    menu.style.bottom = "-300px"
+  let menu = document.querySelector("#location-menu");
+
+  menu.style.bottom = "-300px"
 })
 
 const element1 = document.querySelector("#main");
@@ -134,13 +134,13 @@ const element6 = document.querySelector("#gambar");
 const element7 = document.querySelector("#penutup");
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('animate');
-        } else {
-            entry.target.classList.remove('animate');
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+    } else {
+      entry.target.classList.remove('animate');
+    }
+  });
 })
 
 observer.observe(element1);
